a1f125547f2efbadaba6d85d806fb203946a55b3
Costica Puntaru <pc@everymatrix.com>
2015-11-23 11:29:05 +0200

    task(build): add a build system

---
+++ webpack.config.base.js
@@ -0,0 +1,27 @@
+'use strict';
+var webpack = require('webpack');
+
+var reactExternal = {
+    root: 'React',
+    commonjs2: 'react',
+    commonjs: 'react',
+    amd: 'react'
+};
+
+module.exports = {
+    externals: {
+        'react': reactExternal
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loaders: ['babel-loader?stage=0'], exclude: /node_modules/ }
+        ]
+    },
+    output: {
+        library: 'ReduxFormValidation',
+        libraryTarget: 'umd'
+    },
+    resolve: {
+        extensions: ['', '.js']
+    }
+};
