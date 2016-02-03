78f9e4d60b6a25c58d302594f192604a86ae7c48
Matt Calthrop <matt.calthrop@opencredo.com>
2016-02-03 15:30:56 +0000

    Reformat unit tests

--- src/__tests__/maxLength.spec.js
+++ src/__tests__/maxLength.spec.js
@@ -3,16 +3,16 @@ import { maxLength } from '../basic-validations';
 
 // Note: each validator returns `true` when the `value` is invalid
 describe('Validator: maxLength', function validateMaxLength() {
-  it('should be valid when `value` is `undefined` and `prop` is 3', function () {
+  it('should be valid when `value` is undefined and `prop` is 3', function () {
     assert(maxLength({}, undefined, 3) === false);
   });
-  it('should be invalid when `value` is `1234` and `prop` is 3', function () {
+  it('should be invalid when `value` is "1234" and `prop` is 3', function () {
     assert(maxLength({}, '1234', 3) === true);
   });
-  it('should be valid when `value` is `1234` and `prop` is 4', function () {
+  it('should be valid when `value` is "1234" and `prop` is 4', function () {
     assert(maxLength({}, '1234', 4) === false);
   });
-  it('should be valid when `value` is `1234` and `prop` is 5', function () {
+  it('should be valid when `value` is "1234" and `prop` is 5', function () {
     assert(maxLength({}, '1234', 5) === false);
   });
 });
