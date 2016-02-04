35edde4ca57db14b76e6a980145f52b8c964748f
Matt Calthrop <matt.calthrop@opencredo.com>
2016-02-04 09:59:27 +0000

    Update comments in test

--- src/__tests__/required.spec.js
+++ src/__tests__/required.spec.js
@@ -2,16 +2,16 @@ import assert from 'assert';
 import { required } from '../basic-validations';
 
 describe('Validator: required', function () {
-  it('should return false when `value` is empty and `prop` is false', function () {
+  it('should be valid when `value` is empty and `prop` is false', function () {
     assert(required({}, '', false) === false);
   });
-  it('should return false when `value` is truthy and `prop` is false', function () {
+  it('should be valid when `value` is truthy and `prop` is false', function () {
     assert(required({}, 'true', false) === false);
   });
-  it('should return true when `value` is empty and `prop` is true', function () {
+  it('should be invalid when `value` is empty and `prop` is true', function () {
     assert(required({}, '', true) === true);
   });
-  it('should return false when `value` is truthy and `prop` is true', function () {
+  it('should be valid when `value` is truthy and `prop` is true', function () {
     assert(required({}, 'true', true) === false);
   });
 });
